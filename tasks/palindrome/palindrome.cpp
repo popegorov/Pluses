51ea16fbdf090884d6342360bfc72ada582fdc64
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-01-14 19:09:26 +0300

    First try

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,20 @@
 #include "palindrome.h"
+#include <string>
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    std::string good_str;
+    int str_size = str.size();
+    for (int i = 0; i < str_size; ++i) {
+        if (str[i] != ' ') {
+            good_str.push_back(str[i]);
+        }
+    }
+    int good_size = good_str.size();
+    bool flag = true;
+    for (int i = 0; i < (good_size / 2); ++i) {
+        if (good_str[i] != good_str[good_size - i - 1]) {
+            flag = false;
+        }
+    }
+    return flag;
 }
