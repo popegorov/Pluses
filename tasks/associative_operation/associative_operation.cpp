22d63d38eabc9cd6f39eebab64a828d8121ee574
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-01-17 18:40:04 +0300

    Updated associative_operation

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,5 +1,16 @@
 #include "associative_operation.h"
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    return false;
+    int n = table.size();
+    for (int x = 0; x < n; ++x) {
+        for (int y = 0; y < n; ++y) {
+            for (int z = 0; z < n; ++z) {
+                if (table[table[x][y]][z] != table[x][table[y][z]]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
 }
+
