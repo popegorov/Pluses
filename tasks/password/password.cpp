3c229aa2911c051942f738dbb66a9776fd8a5ab8
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-01-17 23:47:27 +0300

    Updated solution

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -2,9 +2,8 @@
 
 bool ValidatePassword(const std::string& password) {
     int pas_size = password.size();
-    bool flag = true;
     if (pas_size < 8 || pas_size > 14) {
-        flag = false;
+        return false;
     }
     int big = 0;
     int small = 0;
@@ -12,20 +11,20 @@ bool ValidatePassword(const std::string& password) {
     int other = 0;
     for (int i = 0; i < pas_size; ++i) {
         char cur = password[i];
-        if (cur + 0 < 33 || cur + 0 > 126) {
-            flag = false;
-        } else if (cur + 0 >= 48 && cur + 0 <= 57) {
+        if (cur < 33 || cur > 126) {
+            return false;
+        } else if (isdigit(cur)) {
             digit = 1;
-        } else if (cur + 0 >= 65 && cur + 0 <= 90) {
+        } else if (isupper(cur)) {
             big = 1;
-        } else if (cur + 0 >= 97 && cur + 0 <= 122) {
+        } else if (islower(cur)) {
             small = 1;
         } else {
             other = 1;
         }
     }
     if (digit + big + small + other < 3) {
-        flag = false;
+        return false;
     }
-    return flag;
+    return true;
 }
