a1571e0cc8f7cdb4b2ad219ea07ac6f01078273e
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-01-19 18:43:38 +0300

    First try

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,4 +1,21 @@
 #include "sort_students.h"
+#include <algorithm>
+#include <tuple>
+
+bool CompareName(const Student &a, const Student &b) {
+    return std::tie(a.last_name, a.name, a.birth_date.year, a.birth_date.month, a.birth_date.day)
+           < std::tie(b.last_name, b.name, b.birth_date.year, b.birth_date.month, b.birth_date.day);
+}
+
+bool CompareDate(const Student &a, const Student &b) {
+    return std::tie(a.birth_date.year, a.birth_date.month, a.birth_date.day, a.last_name, a.name)
+           < std::tie(b.birth_date.year, b.birth_date.month, b.birth_date.day, b.last_name, b.name);
+}
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
+    if (sortKind == SortKind::Name) {
+        std::sort(students.begin(), students.end(), CompareName);
+    } else {
+        std::sort(students.begin(), students.end(), CompareDate);
+    }
 }
