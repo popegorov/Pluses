7af6490a2e6e3d9e3fe506211902c9831249e8b3
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-01-30 01:21:48 +0300

    Updated solution

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,16 +1,21 @@
 #include "word2vec.h"
 #include <vector>
+#include <limits>
+
+int ScalarMultiplication(const std::vector<int>& first, const std::vector<int>& second) {
+    int sum = 0;
+    for (size_t i = 0; i < first.size(); ++i) {
+        sum += first[i] * second[i];
+    }
+    return sum;
+}
 
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    int cur_sum;
-    int max_sum = -2000000000;
+    int max_sum = std::numeric_limits<int>::min();
     std::vector<std::string> closest_words;
     for (size_t i = 1; i < vectors.size(); ++i) {
-        cur_sum = 0;
-        for (size_t j = 0; j < vectors[i].size(); ++j) {
-            cur_sum += vectors[0][j] * vectors[i][j];
-        }
+        int cur_sum = ScalarMultiplication(vectors[0], vectors[i]);
         if (cur_sum > max_sum) {
             max_sum = cur_sum;
             closest_words = {words[i]};
