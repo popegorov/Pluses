2e113bd49b142c2eb64ecb7d2cca2330eee81cae
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-01-30 01:55:51 +0300

    Second try

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,20 +1,23 @@
 #include "tests_checking.h"
 #include <deque>
+#include <vector>
 
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
     std::deque<std::string> tests;
-    std::vector<std::string> student_check(queries.size());
-    for (size_t i = 0; i < student_actions.size(); ++i) {
-        if (student_actions[i].side == Side::Bottom){
-            tests.push_back(student_actions[i].name);
+    for (const auto& student : student_actions) {
+        if (student.side == Side::Bottom){
+            tests.push_back(student.name);
         } else {
-            tests.push_front(student_actions[i].name);
+            tests.push_front(student.name);
         }
     }
-    for (size_t i = 0; i < queries.size(); ++i) {
-        student_check[i] = tests[queries[i] - 1];
+
+    std::vector<std::string> student_check;
+
+    for (const auto& test_number : queries) {
+        student_check.push_back(tests[test_number - 1]);
     }
     return student_check;
 }
