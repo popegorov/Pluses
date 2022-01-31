76c718db369392dca1c8fa4ced765793e5a72fa1
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-01-31 17:10:49 +0300

    First try

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -1,5 +1,48 @@
 #include "unixpath.h"
+#include <string_view>
+#include <vector>
+
+std::vector<std::string_view> StringViewSplit(std::string_view& str, const std::string_view& elem = "/") {
+    std::vector<std::string_view> result;
+    size_t first = 0;
+    while (first < str.size()) {
+        const auto second = str.find_first_of(elem, first);
+        if (first != second) {
+            result.emplace_back(str.substr(first, second - first));
+        }
+        if (second == std::string_view::npos) {
+            break;
+        }
+        first = second + 1;
+    }
+    return result;
+}
+
+std::vector<std::string_view> PathModification(std::vector<std::string_view>& cur_dir,
+                                               const std::vector<std::string_view>& path) {
+    for (const auto& elem : path) {
+        if (elem == ".." && !cur_dir.empty()) {
+            cur_dir.pop_back();
+        } else if (elem == ".") {
+        } else if (elem != "..") {
+            cur_dir.emplace_back(elem);
+        }
+    }
+    return cur_dir;
+}
 
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
-    return "";
+    std::vector<std::string_view> cur_dir = StringViewSplit(current_working_dir);
+    std::vector<std::string_view> cur_path = StringViewSplit(path);
+    cur_dir = PathModification(cur_dir, cur_path);
+    std::string normal_path;
+    std::string slash = "/";
+    for (const auto& elem : cur_dir) {
+        normal_path += slash;
+        normal_path += elem;
+    }
+    if (normal_path.empty()) {
+        return "/";
+    }
+    return normal_path;
 }
