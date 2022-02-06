5be230c2353a16d5d76f63511e5a9654914c2a7f
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-02-06 16:33:14 +0300

    Fourth try

--- tasks/search/search.cpp
+++ tasks/search/search.cpp
@@ -1,12 +1,11 @@
 #include "search.h"
 #include <cmath>
-#include <set>
-#include <map>
-#include <string>
+#include <string_view>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
-std::vector<std::string_view> StringViewSplit(std::string_view& str, const std::string_view& elem = " ") {
+std::vector<std::string_view> StringViewSplit(std::string_view& str, const std::string_view& elem = "\n") {
     std::vector<std::string_view> result;
     size_t first = 0;
     while (first < str.size()) {
@@ -43,45 +42,76 @@ std::vector<std::string_view> Split(std::string_view text) {
     return words;
 }
 
-std::unordered_map<std::string_view, double> CountIDF(
-    std::vector<std::string_view>& lines, std::unordered_map<std::string_view, std::set<size_t>>& query_count,
-    const std::set<std::string_view>& queries) {
-    for (size_t i = 0; i < lines.size(); ++i) {
-        auto words = Split(lines[i]);
-        for (const auto& word : words) {
-            for (const auto& query : queries) {
-                if (word == query) {
-                    query_count[query].insert(i);
-                    break;
-                }
+struct Hash {
+    size_t operator()(const std::string_view& s) const {
+        size_t h = 1;
+        if (!s.empty()){
+            h = static_cast<size_t>(tolower(s[0]) - 'a' + 1);
+        }
+        size_t h_prev = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            size_t h_new = h;
+            h = ((h_prev * 1009 + static_cast<size_t>(tolower(s[i]) - 'a' + 1)) % static_cast<size_t>(1e9 + 7));
+            h_prev = h_new;
+        }
+        return h;
+    }
+};
+
+struct Equal {
+    bool operator()(const std::string_view& a, const std::string_view& b) const {
+        if (a.size() != b.size()) {
+            return false;
+        }
+        for (size_t i = 0; i < a.size(); ++i) {
+            if (tolower(a[i]) != tolower(b[i])) {
+                return false;
             }
         }
+        return true;
     }
-    std::unordered_map<std::string_view, double> idf(query_count.size());
-    for (const auto& [query, lines_set] : query_count) {
-        double res = static_cast<double>(lines.size()) / static_cast<double>(lines_set.size());
-        idf[query] = log(res);
+};
+
+using HashSet = std::unordered_set<std::string_view, Hash, Equal>;
+
+HashSet FillQuerySet(const std::vector<std::string_view>& queries) {
+    HashSet res;
+    for (const auto& query : queries) {
+        res.insert(query);
     }
-    return idf;
+    return res;
 }
 
-std::map<std::pair<std::string_view, size_t>, double> CountTF(
-    std::vector<std::string_view>& lines, std::unordered_map<std::string_view, std::set<size_t>>& query_count,
-    const std::set<std::string_view>& queries) {
-    std::map<std::pair<std::string_view, size_t>, double> tf;
-    for (const auto& query : queries) {
-        for (const auto& line : query_count[query]) {
-            auto words = Split(lines[line]);
-            size_t cur_cnt = 0;
-            for (const auto& word : words) {
-                if (word == query) {
-                    cur_cnt++;
-                }
+using HashMap = std::unordered_map<std::string_view, size_t, Hash, Equal>;
+
+std::vector<HashMap> FillHashMap(const std::vector<std::string_view>& lines, const HashSet& queries,
+                                 std::vector<size_t>& line_sizes) {
+    std::vector<HashMap> res(lines.size());
+    for (size_t i = 0; i < lines.size(); ++i) {
+        auto words = Split(lines[i]);
+        line_sizes[i] = words.size();
+        for (const auto& word : words) {
+            if (queries.contains(word)) {
+                res[i][word] += 1;
             }
-            tf[{query, line}] = static_cast<double>(cur_cnt) / words.size();
         }
     }
-    return tf;
+    return res;
+}
+
+std::unordered_map<std::string_view, double> CountIDF(std::vector<HashMap>& query_count, const HashSet& queries,
+                                                      const size_t text_size) {
+    std::unordered_map<std::string_view, double> idf(queries.size());
+    for (const auto& query : queries) {
+        size_t cur_cnt = 0;
+        for (auto& hash_map : query_count) {
+            if (hash_map[query]) {
+                cur_cnt++;
+            }
+        }
+        idf[query] = std::log(static_cast<double>(text_size) / cur_cnt);
+    }
+    return idf;
 }
 
 bool ComparePair(const std::pair<double, size_t>& a, const std::pair<double, size_t>& b) {
@@ -91,13 +121,18 @@ bool ComparePair(const std::pair<double, size_t>& a, const std::pair<double, siz
     return a.second < b.second;
 }
 
-std::vector<std::pair<double, size_t>> CountTFIDF(std::map<std::pair<std::string_view, size_t>, double>& tf,
-                                                  std::unordered_map<std::string_view, double>& idf) {
+std::vector<std::pair<double, size_t>> CountTFIDF(std::vector<HashMap>& query_count, HashSet& queries,
+                                                  std::unordered_map<std::string_view, double>& idf,
+                                                  std::vector<std::string_view>& lines,
+                                                  std::vector<size_t>& line_sizes) {
     std::vector<std::pair<double, size_t>> tf_idf;
     std::unordered_map<size_t, double> tf_idf_map;
-    for (const auto& [line, value] : tf) {
-        double res = idf[line.first] * value;
-        tf_idf_map[line.second] += res;
+    for (size_t i = 0; i < query_count.size(); ++i) {
+        for (const auto& query : queries) {
+            if (query_count[i][query]) {
+                tf_idf_map[i] += static_cast<double>(query_count[i][query]) / line_sizes[i];
+            }
+        }
     }
     for (const auto& [line, res] : tf_idf_map) {
         tf_idf.push_back({res, line});
@@ -108,10 +143,15 @@ std::vector<std::pair<double, size_t>> CountTFIDF(std::map<std::pair<std::string
 
 std::vector<std::string_view> CutVector(std::vector<std::pair<double, size_t>>& tf_idf, size_t len,
                                         std::vector<std::string_view>& lines) {
+    if (len > tf_idf.size()) {
+        len = tf_idf.size();
+    }
     std::vector<std::string_view> res;
     for (size_t i = 0; i < len; ++i) {
         if (tf_idf[i].second > 0) {
             res.push_back(lines[tf_idf[i].second]);
+        } else {
+            break;
         }
     }
     return res;
@@ -119,16 +159,17 @@ std::vector<std::string_view> CutVector(std::vector<std::pair<double, size_t>>& 
 
 std::vector<std::string_view> Search(std::string_view text, std::string_view query, size_t results_count) {
     auto queries = Split(query);
-    std::set<std::string_view> set_queries(queries.begin(), queries.end());
-    std::unordered_map<std::string_view, std::set<size_t>> query_count;
+    auto query_set = FillQuerySet(queries);
 
     auto lines = StringViewSplit(text, "\n");
+    size_t text_size = lines.size();
+    std::vector<size_t> line_sizes(text_size);
 
-    auto idf = CountIDF(lines, query_count, set_queries);
+    auto query_count = FillHashMap(lines, query_set, line_sizes);
 
-    auto tf = CountTF(lines, query_count, set_queries);
+    auto idf = CountIDF(query_count, query_set, text_size);
 
-    auto tf_idf = CountTFIDF(tf, idf);
+    auto tf_idf = CountTFIDF(query_count, query_set, idf, lines, line_sizes);
 
     return CutVector(tf_idf, results_count, lines);
 }
