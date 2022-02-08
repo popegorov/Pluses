4b6f71bfcd86b6fa9e13928c4d8c103c36c5d9be
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-02-08 23:24:03 +0300

    First try

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -1,1 +1,40 @@
 #include "scorer.h"
+
+void Scorer::OnCheckFailed(const StudentName& student_name, const TaskName& task_name) {
+    current_results_[{student_name, task_name}].first = true;
+    if (result_.contains(student_name) && result_[student_name].contains(task_name)) {
+        result_[student_name].erase(task_name);
+    }
+}
+
+void Scorer::OnCheckSuccess(const StudentName& student_name, const TaskName& task_name) {
+    current_results_[{student_name, task_name}].first = false;
+    if (!current_results_[{student_name, task_name}].second) {
+        result_[student_name].insert(task_name);
+        current_results_.erase({student_name, task_name});
+    }
+}
+
+void Scorer::OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name) {
+    current_results_[{student_name, task_name}].second = true;
+    if (result_.contains(student_name) && result_[student_name].contains(task_name)) {
+        result_[student_name].erase(task_name);
+    }
+}
+
+void Scorer::OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name) {
+    current_results_[{student_name, task_name}].second = false;
+    if (!current_results_[{student_name, task_name}].first) {
+        result_[student_name].insert(task_name);
+        current_results_.erase({student_name, task_name});
+    }
+}
+
+void Scorer::Reset() {
+    current_results_.clear();
+    result_.clear();
+}
+
+ScoreTable Scorer::GetScoreTable() const {
+    return result_;
+}
--- tasks/scorer2/scorer.h
+++ tasks/scorer2/scorer.h
@@ -11,6 +11,9 @@ using TaskName = std::string;
 using ScoreTable = std::map<StudentName, std::set<TaskName>>;
 
 class Scorer {
+    std::map<std::pair<StudentName, TaskName>, std::pair<bool, bool>> current_results_;
+    ScoreTable result_;
+
 public:
     void OnCheckFailed(const StudentName& student_name, const TaskName& task_name);
     void OnCheckSuccess(const StudentName& student_name, const TaskName& task_name);
