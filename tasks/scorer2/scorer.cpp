1719bb301223cab839ef2553999ff74aa99d7b0b
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-02-09 14:58:43 +0300

    Sixth try

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -1,11 +1,11 @@
 #include "scorer.h"
 
 void Scorer::OnCheckFailed(const StudentName& student_name, const TaskName& task_name) {
-    current_results_[{student_name, task_name}].first = true;
+    current_results_[{student_name, task_name}].first = false;
 }
 
 void Scorer::OnCheckSuccess(const StudentName& student_name, const TaskName& task_name) {
-    current_results_[{student_name, task_name}].first = false;
+    current_results_[{student_name, task_name}].first = true;
 }
 
 void Scorer::OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name) {
@@ -23,7 +23,7 @@ void Scorer::Reset() {
 ScoreTable Scorer::GetScoreTable() const {
     ScoreTable result;
     for (const auto& [name_task, check_merge] : current_results_) {
-        if (!check_merge.first && !check_merge.second) {
+        if (check_merge.first && !check_merge.second) {
             result[name_task.first].insert(name_task.second);
         }
     }
