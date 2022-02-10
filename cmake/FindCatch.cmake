54df2c02858dc7fbb6350a89eaffb342185ee90f
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-02-10 21:58:39 +0300

    Third try

--- cmake/FindCatch.cmake
+++ cmake/FindCatch.cmake
@@ -1,5 +1,5 @@
 add_library(contrib_catch_main
-  contrib/catch/catch_main.cpp ../tasks/search2/split.h ../tasks/search2/split.cpp ../tasks/search2/hashing.h ../tasks/search2/hashing.cpp)
+  contrib/catch/catch_main.cpp)
 
 target_include_directories(contrib_catch_main
   PUBLIC contrib/catch)
