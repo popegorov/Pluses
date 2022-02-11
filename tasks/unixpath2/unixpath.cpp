81d7e1cdec1a1aa37a39657171d9ea90cd296f98
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-02-11 13:21:39 +0300

    Fourth try

--- tasks/unixpath2/unixpath.cpp
+++ tasks/unixpath2/unixpath.cpp
@@ -2,7 +2,7 @@
 #include <string_view>
 #include <vector>
 
-std::vector<std::string_view> StringViewSplit(std::string_view& str, const std::string_view& elem = "/") {
+std::vector<std::string_view> Split(std::string_view& str, const std::string_view& elem = "/") {
     std::vector<std::string_view> result;
     size_t first = 0;
     while (first < str.size()) {
@@ -18,8 +18,29 @@ std::vector<std::string_view> StringViewSplit(std::string_view& str, const std::
     return result;
 }
 
+std::vector<std::string_view> Normalize(const std::vector<std::string_view>& path) {
+    std::vector<std::string_view> res;
+    if (path[0] == ".") {
+        res.push_back(".");
+    }
+    for (const auto& elem : path) {
+        if (elem == ".." && res.size() == 1 && res.back() == ".") {
+            res.pop_back();
+            res.push_back(elem);
+        } else if (elem == ".." && !res.empty() && res.back() != "..") {
+            res.pop_back();
+        } else if (elem != ".") {
+            res.push_back(elem);
+        }
+    }
+    return res;
+}
+
 std::string PathToString(std::vector<std::string_view> path) {
-    std::string result;
+    if (path.empty()) {
+        return "";
+    }
+    std::string result = static_cast<std::string>(path[0]);
     for (size_t i = 1; i < path.size(); ++i) {
         result += "/";
         result += path[i];
@@ -27,85 +48,65 @@ std::string PathToString(std::vector<std::string_view> path) {
     return result;
 }
 
-void ChangeAbsolutePath(const std::vector<std::string_view>& path, std::vector<std::string_view>& result,
-                        bool flag = false) {
+std::vector<std::string_view> ChangeAbsolutePath(const std::vector<std::string_view>& path,
+                                                 std::vector<std::string_view>& result, bool flag = false) {
     if (flag) {
         result.clear();
     }
     for (const auto& elem : path) {
-        if (elem == ".." && !result.empty()) {
-            result.pop_back();
-        } else if (elem != ".." && elem != ".") {
-            result.push_back(elem);
-        }
+        result.push_back(elem);
     }
+    result = Normalize(result);
+    return result;
 }
 
-void ChangeRelativePath(const std::vector<std::string_view>& path, const std::vector<std::string_view>& cur_dir,
-                        std::vector<std::string_view>& relative_path, bool flag) {
-    if (flag) {
-        size_t len = cur_dir.size();
-        if (len > path.size()) {
-            len = path.size();
+size_t BuildIndex(const std::vector<std::string_view>& cur_dir, const std::vector<std::string_view>& absolute_path) {
+    size_t len = cur_dir.size();
+    if (len > absolute_path.size()) {
+        len = absolute_path.size();
+    }
+    size_t max_i = cur_dir.size();
+    for (size_t i = 0; i < len; ++i) {
+        if (absolute_path[i] != cur_dir[i]) {
+            max_i = i;
+            break;
         }
-        size_t max_i = cur_dir.size();
-        for (size_t i = 0; i < len; ++i) {
-            if (path[i] != cur_dir[i]) {
-                max_i = i;
-                break;
-            }
-        }
-        if (max_i == cur_dir.size()) {
-            relative_path = {"."};
-        } else {
-            relative_path.clear();
-            for (size_t i = 0; i < (cur_dir.size() - max_i); ++i) {
-                relative_path.push_back("..");
-            }
-        }
-        for (size_t i = max_i; i < path.size(); ++i) {
-            relative_path.push_back(path[i]);
-        }
+    }
+    return max_i;
+}
+
+void ChangeRelativePath(const std::vector<std::string_view>& absolute_path,
+                        const std::vector<std::string_view>& cur_dir, std::vector<std::string_view>& relative_path) {
+    auto max_i = BuildIndex(cur_dir, absolute_path);
+    if (max_i == cur_dir.size()) {
+        relative_path = {"."};
     } else {
-        for (const auto& elem : path) {
-            if (elem == ".." && !relative_path.empty() && relative_path.size() != 1) {
-                relative_path.pop_back();
-                if (relative_path.empty()) {
-                    relative_path = {"."};
-                }
-            } else if (elem == ".." && !relative_path.empty() && relative_path[0] == ".") {
-                relative_path = {".."};
-            } else if (elem == ".." && !relative_path.empty() && relative_path[0] == "..") {
-                relative_path.push_back("..");
-            } else if (elem != ".." && elem != ".") {
-                relative_path.push_back(elem);
-            }
+        relative_path.clear();
+        for (size_t i = 0; i < (cur_dir.size() - max_i); ++i) {
+            relative_path.push_back("..");
         }
+    }
+    for (size_t i = max_i; i < absolute_path.size(); ++i) {
+        relative_path.push_back(absolute_path[i]);
     }
 }
 
 UnixPath::UnixPath(std::string_view initial_dir) {
-    std::vector<std::string_view> res;
-    ChangeAbsolutePath(StringViewSplit(initial_dir), res);
-    cur_dir_ = res;
-    absolute_path_ = {cur_dir_.begin(), cur_dir_.end()};
+    cur_dir_ = absolute_path_ = Normalize(Split(initial_dir));
     relative_path_ = {"."};
 }
 
 void UnixPath::ChangeDirectory(std::string_view initial_directory) {
     bool first_sym_is_slash = (!initial_directory.empty() && initial_directory[0] == '/');
-    auto path = StringViewSplit(initial_directory);
-    ChangeRelativePath(path, cur_dir_, relative_path_, first_sym_is_slash);
+    auto path = Split(initial_directory);
     ChangeAbsolutePath(path, absolute_path_, first_sym_is_slash);
+    ChangeRelativePath(absolute_path_, cur_dir_, relative_path_);
 }
 
 std::string UnixPath::GetAbsolutePath() const {
-    if (absolute_path_.empty()) {
-        return "/";
-    }
-    return "/" + static_cast<std::string>(absolute_path_[0]) + PathToString(absolute_path_);
+    return "/" + PathToString(absolute_path_);
 }
 
 std::string UnixPath::GetRelativePath() const {
-    return static_cast<std::string>(relative_path_[0]) + PathToString(relative_path_);
+    return PathToString(relative_path_);
 }
