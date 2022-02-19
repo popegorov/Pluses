b9f7112d29630b18d7d59faec21f15fc92d3a7f5
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2022-02-19 19:35:49 +0300

    First try

--- tasks/poly/poly.cpp
+++ tasks/poly/poly.cpp
@@ -1,1 +1,146 @@
 #include "poly.h"
+
+#include <cmath>
+#include <string>
+
+void Poly::NewPoly(std::vector<std::pair<size_t, int>>& coef) {
+    for (const auto& elem : coef) {
+        if (elem.second != 0) {
+            coefficients_[elem.first] = elem.second;
+        }
+    }
+}
+
+Poly::Poly() {
+    std::vector<std::pair<size_t, int>> coef;
+    NewPoly(coef);
+}
+
+Poly::Poly(const std::vector<int>& coefficients) {
+    std::vector<std::pair<size_t, int>> coef;
+    for (size_t i = 0; i < coefficients.size(); ++i) {
+        coef.push_back({i, coefficients[i]});
+    }
+    NewPoly(coef);
+}
+
+Poly::Poly(const std::vector<std::pair<size_t, int>>& coefficients) {
+    auto coef_copy = coefficients;
+    NewPoly(coef_copy);
+}
+
+Poly::Poly(const Poly& other) {
+    std::vector<std::pair<size_t, int>> coef;
+    for (const auto& elem : other.coefficients_) {
+        coef.push_back(elem);
+    }
+    NewPoly(coef);
+}
+
+Poly& Poly::operator=(const Poly& other) {
+    if (this == &other) {
+        return *this;
+    }
+    coefficients_ = other.coefficients_;
+    return *this;
+}
+
+int64_t Poly::operator()(int x) const {
+    int64_t res = 0;
+
+    for (const auto& [deg, coef] : coefficients_) {
+        res += static_cast<int64_t>(coef) * static_cast<int64_t>(pow(x, deg));
+    }
+
+    return res;
+}
+
+bool Poly::operator==(const Poly& other) const {
+    return coefficients_ == other.coefficients_;
+}
+
+bool Poly::operator!=(const Poly& other) const {
+    return coefficients_ != other.coefficients_;
+}
+
+Poly Poly::operator-() const {
+    Poly result;
+    for (const auto& [deg, coef] : coefficients_) {
+        result.coefficients_[deg] = -coef;
+    }
+
+    return result;
+}
+
+Poly& Poly::operator+=(const Poly& other) {
+    for (const auto& [deg, coef] : other.coefficients_) {
+        coefficients_[deg] += coef;
+        if (!coefficients_[deg]) {
+            coefficients_.erase(deg);
+        }
+    }
+
+    return *this;
+}
+
+Poly& Poly::operator-=(const Poly& other) {
+    return (*this += -other);
+}
+
+Poly Poly::operator+(const Poly& other) const {
+    Poly result = *this;
+    result += other;
+
+    return result;
+}
+
+Poly Poly::operator-(const Poly& other) const {
+    Poly result = *this;
+    result -= other;
+
+    return result;
+}
+
+Poly Poly::operator*(const Poly& other) const {
+    Poly result;
+    for (const auto& [deg1, coef1] : coefficients_) {
+        for (const auto& [deg2, coef2] : other.coefficients_) {
+            result.coefficients_[deg1 + deg2] += coef1 * coef2;
+        }
+    }
+
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& out, const Poly& p) {
+    out << "y = ";
+    if (p.coefficients_.empty()) {
+        out << 0;
+        return out;
+    }
+    for (auto it = p.coefficients_.rbegin(); it != p.coefficients_.rend(); ++it) {
+        if (it->second < 0) {
+            out << "- ";
+        } else if (it != p.coefficients_.rbegin()) {
+            out << "+ ";
+        }
+
+        if ((it->first) > 1) {
+            if (it->second != 1 && it->second != -1) {
+                out << abs(it->second) << "x^" << it->first << " ";
+            } else {
+                out << "x^" << it->first << " ";
+            }
+        } else if (it->first) {
+            if (it->second != 1 && it->second != -1) {
+                out << abs(it->second) << "x ";
+            } else {
+                out << "x ";
+            }
+        } else {
+            out << abs(it->second);
+        }
+    }
+
+    return out;
+}
